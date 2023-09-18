2a921a474f95de60796f839f2d10b8c2ec4a0420
Jasmin Sakanovic <jsakanov@c3r3p6.42vienna.com>
2023-09-18 19:31:16 +0200

    Update 7

---
+++ ft_split.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsakanov <jsakanov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/16 13:45:57 by jsakanov          #+#    #+#             */
+/*   Updated: 2023/09/16 22:18:11 by jsakanov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+static int	ft_count_substrings(char const *s, char c)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c || s[i] == ' ' || s[i] == '|')
+			i++;
+		else
+		{
+			j++;
+			while (s[i] && s[i] != c)
+				i++;
+		}
+	}
+	return (j);
+}
+
+static char	*ft_split_substring(char const *start, char const *finish)
+{
+	int		len;
+	char	*subs;
+	int		i;
+
+	i = 0;
+	len = finish - start;
+	subs = (char *)malloc((len + 1) * sizeof(char));
+	if (!subs)
+		return (NULL);
+	while (start < finish)
+	{
+		subs[i] = *start;
+		i++;
+		start++;
+	}
+	subs[i] = '\0';
+	return (subs);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int			subs_count;
+	char		**s1;
+	char const	*start;
+	int			i;
+
+	subs_count = ft_count_substrings(s, c);
+	s1 = (char **)malloc((subs_count + 1) * sizeof(char *));
+	if (!s1)
+		return (NULL);
+	start = s;
+	i = 0;
+	while (*s)
+	{
+		if (*s == c)
+		{
+			if (s != start)
+				s1[i++] = ft_split_substring(start, s);
+			start = s + 1;
+		}
+		s++;
+	}
+	if (s != start)
+		s1[i++] = ft_split_substring(start, s);
+	s1[i] = 0;
+	return (s1);
+}
+/*
+#include <stdio.h>
+int main(void) 
+{
+	char	str[400] = "Jujutsu Kaisen Kaisen Kasien";
+    char	**result = ft_split(str, 'i');
+	int		i;
+
+	i = 0;
+	if (result) 
+	{
+		while (result[i] != NULL)
+		{
+			printf("%s\n", result[i]);
+			//free(result[i]);
+			i++;
+		}
+		ft_free_mem(result);
+	}
+	return 0;
+}
+*/
